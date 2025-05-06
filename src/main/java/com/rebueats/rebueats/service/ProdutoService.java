c983d892a648a507904966d2094caa42f6b3d5ca
Fatima Oliveira <fatimaolliveira428@gmail.com>
2025-05-06 16:24:54 -0300

    produto service

---
+++ src/main/java/com/rebueats/rebueats/service/ProdutoService.java
@@ -0,0 +1,68 @@
+package com.rebueats.rebueats.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
+
+
+import com.rebueats.rebueats.model.Produto;
+import com.rebueats.rebueats.repository.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+
+	public Produto cadastrarProduto(Produto produto) {
+		return produtoRepository.save(produto);
+	}
+
+	public Optional<Produto> buscarPorId(Long id) {
+		return produtoRepository.findById(id);
+	}
+
+	public List<Produto> buscarPorNome(String nome) {
+		return produtoRepository.findAllByNomeContainingIgnoreCase(nome);
+	}
+
+	public List<Produto> listarProdutos() {
+		return produtoRepository.findAll();
+	}
+	
+	public Optional<Produto> atualizarProduto (Produto produto) {
+		return produtoRepository.findById(produto.getId()).map(produtoExistente -> {
+			
+			produtoExistente.setNome(produto.getNome());
+			produtoExistente.setDescricao(produto.getDescricao());
+			produtoExistente.setPreco(produto.getPreco());
+			produtoExistente.setFoto(produto.getFoto());
+			
+			return produtoRepository.save(produtoExistente);
+		});
+	}
+	
+	public void deletarProduto (Long id) {
+		
+		Optional<Produto> produtoExistente = produtoRepository.findById(id);
+		
+		if (produtoExistente.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto não encontrado!");
+		}
+		
+		produtoRepository.deleteById(id);
+		
+	}
+
+}
+
+
