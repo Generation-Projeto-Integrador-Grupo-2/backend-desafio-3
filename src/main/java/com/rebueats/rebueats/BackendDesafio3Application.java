8cb9c2117b9e6be91388f91bb93ee2f01003e5e9
Caio <caios6356@gmail.com>
2025-05-06 10:05:22 -0300

    configuração inicial

---
+++ src/main/java/com/rebueats/rebueats/BackendDesafio3Application.java
@@ -0,0 +1,13 @@
+package com.rebueats.rebueats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDesafio3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDesafio3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rebueats/rebueats/BackendDesafio3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebueats.rebueats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDesafio3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
