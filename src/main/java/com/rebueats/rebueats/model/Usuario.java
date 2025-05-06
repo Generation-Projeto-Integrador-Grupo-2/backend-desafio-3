c4841f565b44d15180e2eb7a67044a75e4843635
leodipaula <leofernandes9@gnail.com>
2025-05-06 14:24:57 -0300

    inserido anotações da tabela

--- src/main/java/com/rebueats/rebueats/model/Usuario.java
+++ src/main/java/com/rebueats/rebueats/model/Usuario.java
@@ -3,15 +3,20 @@ package com.rebueats.rebueats.model;
 import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+@Entity
+@Table(name = "tb_usuarios")
 public class Usuario {
 
     @Id
